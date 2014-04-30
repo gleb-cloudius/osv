4e67224f5d05c1fef103b91dc6267c279cddfc2e
Gleb Natapov <gleb@cloudius-systems.com>
2014-04-30 11:56:09 +0300

    misc-arc-eviction

--- build.mk
+++ build.mk
@@ -325,6 +325,7 @@ tests += tests/misc-malloc.so
 tests += tests/misc-memcpy.so
 tests += tests/misc-free-perf.so
 tests += tests/tst-fallocate.so
+tests += tests/misc-arc-eviction.so
 endif
 
 tests/hello/Hello.class: javabase=tests/hello
